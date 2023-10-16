61eb1ebf44247a5ed39d7059474432f65d8cdedf
Рогаткин Роман <RRogatkin@alfabank.ru>
2023-10-16 19:42:22 +0300

    added url session

--- HSE Punk Project/ViewController.swift
+++ HSE Punk Project/ViewController.swift
@@ -12,6 +12,21 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard
+                let data = data,
+                let response = response,
+                error == nil
+            else {
+                return
+            }
+//            let decoder = JSONDecoder()
+            // Обработка данных
+            let str = String(data: data, encoding: .utf8)
+            print("Полученные данные: \(str ?? "")")
+        }).resume()
+
     }
 }
 
