97f514afc88c361f7488357800c3714200c99068
Рогаткин Роман <RRogatkin@alfabank.ru>
2023-10-30 19:10:37 +0300

    move BeerDTO to file

--- HSE Punk Project/BeerTable/ViewController.swift
+++ HSE Punk Project/BeerTable/ViewController.swift
@@ -51,28 +51,5 @@ final class ViewController: UIViewController, UITableViewDataSource {
             spinnerView.centerYAnchor.constraint(equalTo: tableView.centerYAnchor)
         ])
         spinnerView.startAnimating()
-
-        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
-        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
-            guard let data = data, error == nil else { return }
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            // Обработка данных
-            self.beerData = try! decoder.decode([BeerDTO].self, from: data)
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-                self.spinnerView.stopAnimating()
-            }
-        }).resume()
     }
 }
-
-
-
-struct BeerDTO: Decodable {
-    let id: Int
-    let name: String
-    let tagline: String
-    let imageUrl: URL
-}
-
