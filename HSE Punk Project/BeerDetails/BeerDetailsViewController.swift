2edb86c372220c0365d184fa603ee5db2643fcb7
Рогаткин Роман <RRogatkin@alfabank.ru>
2023-11-13 20:18:21 +0300

    added BeerDetails

---
+++ HSE Punk Project/BeerDetails/BeerDetailsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  BeerDetailsViewController.swift
+//  HSE Punk Project
+//
+//  Created by Рогаткин Роман on 13.11.2023.
+//
+
+import UIKit
+
+final class BeerDetailsViewController: UIViewController {
+
+    var beerModel: BeerDTO?
+
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        return view
+    }()
+
+    private lazy var titleView: UILabel = {
+        let view = UILabel()
+        return view
+    }()
+
+    private lazy var subtitleView: UILabel = {
+        let view = UILabel()
+        return view
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        addSubviews()
+        makeConstraints()
+        configure()
+    }
+
+    func addSubviews() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(titleView)
+        stackView.addArrangedSubview(subtitleView)
+    }
+
+    func makeConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    func configure() {
+        titleView.text = beerModel?.name
+        subtitleView.text = beerModel?.tagline
+    }
+}
