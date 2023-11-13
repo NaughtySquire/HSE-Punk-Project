ae624a55152ebbd06b0d0c5c6c716a7f6287ef6a
Рогаткин Роман <RRogatkin@alfabank.ru>
2023-11-13 20:18:40 +0300

    routing to beerDetails implemented

--- HSE Punk Project/BeerTable/BeerTableViewController.swift
+++ HSE Punk Project/BeerTable/BeerTableViewController.swift
@@ -42,6 +42,8 @@ final class BeerTableViewController: UIViewController {
 
 extension BeerTableViewController: BeerTableViewDelegate {
     func didSelectRow(_ beerModel: BeerDTO) {
-
+        let vc = BeerDetailsViewController()
+        vc.beerModel = beerModel
+        present(vc, animated: true)
     }
 }
